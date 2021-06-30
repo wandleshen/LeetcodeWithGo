6c433575ab567dda64f01804d21747a7f8bb567b
wandleshen <wandleshen@zju.edu.cn>
2021-06-30 22:52:34 +0800

    6.30配额，考试歇逼，难受

---
+++ 15.三数之和.go
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=golang
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	ans, start, end, index, addNum, length := make([][]int, 0), 0, 0, 0, 0, len(nums)
+	for index = 1; index < length-1; index++ {
+		start, end = 0, length-1
+		if index > 1 && nums[index] == nums[index-1] {
+			start = index - 1
+		}
+		for start < index && end > index {
+			if start > 0 && nums[start] == nums[start-1] {
+				start++
+				continue
+			}
+			if end < length-1 && nums[end] == nums[end+1] {
+				end--
+				continue
+			}
+			addNum = nums[start] + nums[end] + nums[index]
+			if addNum == 0 {
+				ans = append(ans, []int{nums[start], nums[index], nums[end]})
+				start++
+				end--
+			} else if addNum > 0 {
+				end--
+			} else {
+				start++
+			}
+		}
+	}
+	return ans
+}
+
+// @lc code=end
+
