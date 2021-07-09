fed37458c1c1616480427296cc6d497b2e31dc42
wandleshen <wandleshen@zju.edu.cn>
2021-07-09 23:37:25 +0800

    7.9配额，最近在搞OpenGL，刷题有点懈怠，不过递归依旧yyds

---
+++ 23.合并k个升序链表.go
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=golang
+ *
+ * [23] 合并K个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+	length := len(lists)
+	if length < 1 {
+		return nil
+	}
+	if length == 1 {
+		return lists[0]
+	}
+	num := length / 2
+	left := mergeKLists(lists[:num])
+	right := mergeKLists(lists[num:])
+	return mergeTwoLists(left, right)
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
+
+}
+
+// @lc code=end
+
