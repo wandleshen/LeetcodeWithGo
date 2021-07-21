b6b8d7a27dca89f9253d2764d5a383862ded673f
wandleshen <wandleshen@zju.edu.cn>
2021-07-21 22:55:24 +0800

    7.21更新，之前比较忙，现在终于想起来还有这么个东西，递归yyds

---
+++ 24.两两交换链表中的节点.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=golang
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	first := head
+	second := head.Next
+	other := swapPairs(second.Next)
+	second.Next = first
+	first.Next = other
+	return second
+}
+
+// @lc code=end
+
