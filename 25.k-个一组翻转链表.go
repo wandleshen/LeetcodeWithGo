5f3a7e286338da627a356bcf291def6ea011294b
root <root@DESKTOP-PIEGI8P.localdomain>
2022-03-23 20:39:42 +0800

    NOOOOOOOO

---
+++ 25.k-个一组翻转链表.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=golang
+ *
+ * [25] K 个一组翻转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	node := head
+	for i := 0; i < k; i++ {
+		if node == nil {
+			return head
+		}
+		node = node.Next
+	}
+	newHead := reverse(head, node)
+	head.Next = reverseKGroup(node, k)
+	return newHead
+}
+
+func reverse(head *ListNode, tail *ListNode) *ListNode {
+	prev := tail
+	for head != tail {
+		tmp := head.Next
+		head.Next = prev
+		prev = head
+		head = tmp
+	}
+	return prev
+}
+
+// @lc code=end
+
