be44270363ae2cc0fe933efa22bfc092a367ab53
root <root@DESKTOP-PIEGI8P.localdomain>
2022-04-05 20:22:10 +0800

    4.5

---
+++ 28.实现-str-str.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=golang
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+func strStr(haystack string, needle string) int {
+	for i := 0; ; i++ {
+		for j := 0; ; j++ {
+			if j == len(needle) {
+				return i
+			}
+			if i+j == len(haystack) {
+				return -1
+			}
+			if haystack[i+j] != needle[j] {
+				break
+			}
+		}
+	}
+	return -1
+}
+
+// @lc code=end
+
